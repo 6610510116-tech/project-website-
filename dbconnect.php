f21dae04617fc3493f14b3a56a5314cdb6b3837a
R <pjoy2517@gmail.com>
2025-10-06 16:46:58 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -3,5 +3,6 @@
     $db_username = "root";
     $db_password = "";
     $db_name = "simpledb";
-    $conn = mysqli_connect($host, $db_username, $db_password, $dbname) or die("Could not connect to database" . mysqli_error ($conn));
+    $conn = mysqli_connect($host, $db_username, $db_password, $db_name) or die("Could not connect to database" . mysqli_error ($conn));
+
 ?>
