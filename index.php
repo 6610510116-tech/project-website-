1304f1bdadc4825548a42b4e4fc0156d93d6edf1
chanoknanchiakhong-cmd <chanoknanchiakhong@gmail.com>
2025-10-06 16:40:13 +0700

    ลบ index.html และสร้าง index.php แทน

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1? style="color: blue">
+    <?php
+        echo "Hello, Wolrd!";
+    ?>
+</h1>
+<?php
+
+echo "<p style='color: blue'>This is a paragraph generated bt PHP.</p>";
+
+?>
+
+
+
+<?php
+
+$vat 1.07;
+
+$price = 150;
+
+echo "<p style='color-green'>Price including VAT: " ($price* $vat) THB</p>";
+
+ ?>
+
+
+
+<?php
+
+txt "this is \"Anfield\", \$the home";
+
+echo "<h1 style='color: red">$txt</h1>";
+
+ ?>
+
+</body>
+
+ </html>
